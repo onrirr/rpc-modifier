9695c0116ee72ff2e5b311a5c70a407d52091f2a
onrirr <ulughann@gmail.com>
2024-02-02 22:51:13 +0300

    Refactor app.go and modify the modify function

--- app.go
+++ app.go
@@ -21,36 +21,20 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-	go runner()
+
+}
+
+func modify(id string, activity client.Activity) bool {
+	err := client.Login(id)
+	if err != nil {
+		return false
+	}
+
+	err = client.SetActivity(activity)
+	return err == nil
 }
 
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
-}
-
-func runner() {
-
-	err := client.Login("810540985032900648")
-	if err != nil {
-		panic(err)
-	}
-
-	err = client.SetActivity(client.Activity{
-		State:      "Heyy!!!",
-		Details:    "I'm running on rich-go :)",
-		LargeImage: "largeimageid",
-		LargeText:  "This is the large image :D",
-		SmallImage: "smallimageid",
-		SmallText:  "And this is the small image",
-		Party: &client.Party{
-			ID:         "-1",
-			Players:    15,
-			MaxPlayers: 24,
-		},
-	})
-
-	if err != nil {
-		panic(err)
-	}
 }
